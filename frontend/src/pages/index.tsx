bf4769ea14a6a9344f4557e7a1393c431875f0c0
Podskio <podskio1@gmail.com>
2024-01-26 19:35:29 -0800

    Home page first steps

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,8 +2,14 @@ import "../styles/index.css";
 
 function App() {
   return (
-    <div>
+    <div id="main">
       <h1>AntLogic</h1>
+      <p>
+        Bumi is an anteater at the North Florida Wildlife Center. He’s recently
+        been learning logic and needs you anteaters to help him! The goal is to
+        build ant logic gates that feed him only under certain conditions.
+      </p>
+      <button className="btn-green">PLAY</button>
     </div>
   );
 }
