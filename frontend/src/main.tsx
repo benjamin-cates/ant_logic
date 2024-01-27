fe06171d4a5911669d104e95a0cb58b3512ef1d4
Podskio <podskio1@gmail.com>
2024-01-27 14:12:47 -0800

    Format

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,14 +2,14 @@ import "./page.css";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import App from "./pages/index.tsx";
-import LevelOne from "./pages/levels/1.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Levels from "./pages/levels/index.tsx";
 import Level from "./components/Level.tsx";
+import App from "./pages/index.tsx";
+import Levels from "./pages/levels/index.tsx";
 import Login from "./pages/login/index.tsx";
 import Signup from "./pages/signup/index.tsx";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter basename="/">
