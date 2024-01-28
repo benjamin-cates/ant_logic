43c92eb91e8f197250180d91de3b685691b682eb
Benjamin Cates <thebenjicat@gmail.com>
2024-01-28 09:34:15 -0800

    Change front page message

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -36,9 +36,7 @@ function App() {
         <h1>AntLogic</h1>
         <p>
           Bumi is an anteater at the North Florida Wildlife Center. He's
-          recently been learning logic and needs you anteaters to help him! The
-          goal is to build ant logic gates that feed him only under certain
-          conditions.
+          recently been learning logic and needs you anteaters to help him!
         </p>
         <div className="main-btns">
           <div style={{display: "flex", flexDirection: "row", gap: "0.8rem", width: "30rem", justifyContent: "center"}}>
